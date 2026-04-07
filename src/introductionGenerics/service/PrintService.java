ea68c202f8470dc04fc8883dd8978ff2b629f695
RaposoBrendo <raposobrendo@gmail.com>
2026-04-07 17:01:20 -0300

    Beginning Generics

---
+++ src/introductionGenerics/service/PrintService.java
@@ -0,0 +1,31 @@
+package introductionGenerics.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService {
+
+    List<Integer> list = new ArrayList<>();
+
+    public void addValue(int value){
+        list.add(value);
+    }
+
+    public Integer first(){
+        if(list.isEmpty()){
+            throw new IllegalStateException("List is empty");
+        }
+        return list.get(0);
+    }
+
+    public void print(){
+        System.out.println("[");
+        if(!list.isEmpty()){
+            System.out.println(list.getFirst());
+        }
+        for(int i = 1; i < list.size(); i++){
+            System.out.print(", " + list.get(i));
+        }
+        System.out.println("[");
+    }
+}
