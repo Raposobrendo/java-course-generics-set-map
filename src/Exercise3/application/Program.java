14d64a3963de96b0792fc433602074498fbb743c
RaposoBrendo <raposobrendo@gmail.com>
2026-04-17 17:00:10 -0300

    Final Map Exercise finished. Chapter end.

--- src/Exercise3/application/Program.java
+++ src/Exercise3/application/Program.java
@@ -9,16 +9,31 @@ public class Program {
     public static void main(String[] Args){
 
 
-        Map<String, Double> candidates = new HashMap<>();
+        Map<String, Integer> candidates = new HashMap<>();
 
         String path = "C:\\temp\\in4.txt";
 
         try(BufferedReader br = new BufferedReader(new FileReader(path))){
 
+            String line = br.readLine();
+            while(line != null){
+
+                String[] fields = line.split(",");
+                if (!candidates.containsKey(fields[0])){
+                    candidates.put(fields[0], Integer.parseInt(fields[1]));
+                } else {
+                    candidates.put(fields[0], candidates.get(fields[0]) + Integer.parseInt(fields[1]));
+                }
+
+                line = br.readLine();
+            }
+
+            for(Map.Entry<String, Integer> entry : candidates.entrySet()){
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+
         } catch (Exception e){
             System.out.println("Error: " + e);
         }
-
-
     }
 }
